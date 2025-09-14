e0b8ddc84679862e8f4c7de5860a48eff81639de
KhatijaK05 <honeyblade.journey@gmail.com>
2025-09-14 23:02:16 +0530

    Add files via upload

---
+++ control_struct_task1.py
@@ -0,0 +1,8 @@
+a = int(input('Enter the number:'))
+if (a % 2 == 0):
+    print(f"{a} is an even number")
+elif(a % 2 != 0):
+    print(f"{a} is an odd number")
+else:
+    print('None')
+print('Thank you!')
---
+++ control_struct_task2.py
@@ -0,0 +1,4 @@
+count = 0
+for i in (range(0,50)):
+    count += i+1
+print(f'The sum of numbers from 1 to 50 is : {count}')
